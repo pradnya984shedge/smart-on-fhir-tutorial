fbce84499f8a4d8f3754491976ae402f66c88ee7
Pradnya <Pradnya@6B-PC-133>
2020-03-25 18:39:19 +0000

    added new changes

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -60,7 +60,7 @@
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-          ret.resolve(patient);
+          ret.resolve(p,patient);
         });
       } else {
         onError();
