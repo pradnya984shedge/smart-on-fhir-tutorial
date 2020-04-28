5e4d5c35bd42ad11b70270f55a1f6eebbbdaf7a6
prafull shedge <prafullshedge@gmail.com>
2020-04-28 22:43:37 +0100

    #add new changes

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -117,15 +117,15 @@
 	window.drawVisualization = function (p) {
 		$('#holder').show();
 		$('#loading').hide();
-		//$('#fname').html(p.fname);
-		//$('#lname').html(p.lname);
-		//$('#gender').html(p.gender);
-		//$('#birthdate').html(p.birthdate);
-		//$('#height').html(p.height);
-		//$('#systolicbp').html(p.systolicbp);
-		//$('#diastolicbp').html(p.diastolicbp);
-		//$('#ldl').html(p.ldl);
-		//$('#hdl').html(p.hdl);
+		$('#fname').html(p.fname);
+		$('#lname').html(p.lname);
+		$('#gender').html(p.gender);
+		$('#birthdate').html(p.birthdate);
+		$('#height').html(p.height);
+		$('#systolicbp').html(p.systolicbp);
+		$('#diastolicbp').html(p.diastolicbp);
+		$('#ldl').html(p.ldl);
+		$('#hdl').html(p.hdl);
 	};
 
 })(window);
